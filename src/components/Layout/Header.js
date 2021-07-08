cc8dc888681715caf6f8a1960e09c26cef14b0bf
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-07-08 14:51:18 -0500

    added cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
 
+import HeaderCartButton from './HeaderCartButton';
 // import mealsImage from '../../assets/IMG_8457.HEIC'
-import classes from './Header.module.css'
+import classes from "./Header.module.css";
 
 const Header = (props) => {
-    return (
-        <React.Fragment>
-            <header className={classes.header}>
-                <h1>ReactMeals</h1>
-                <button>Cart</button>
-            </header>
-            <div className={classes['main-image']}>
-                <img src="https://post.healthline.com/wp-content/uploads/2020/07/pizza-beer-1200x628-facebook-1200x628.jpg" alt="Pizza and beer"/>
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img
+          src="https://post.healthline.com/wp-content/uploads/2020/07/pizza-beer-1200x628-facebook-1200x628.jpg"
+          alt="Pizza and beer"
+        />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Header;
