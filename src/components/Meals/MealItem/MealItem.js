f4a73632b1d8f3507cb0ae2bb088067086e3dcc0
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-07-08 16:14:48 -0500

    added input componenet and meal item form componenet

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,5 @@
+
+import MealItemForm from './MealItemForm'
 import classes from './MealItem.module.css'
 
 const MealItem = (props) => {
@@ -12,7 +14,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                
+                <MealItemForm />
             </div>
         </li>
     )
