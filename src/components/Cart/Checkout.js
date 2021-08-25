66e68e1f2b5b913571219dd410cb1142906474d3
Tina Grissom <tinagrissom@yahoo.com>
2021-08-25 15:55:06 -0500

    set up post route to send user data to firebase

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -47,7 +47,13 @@ const Checkout = (props) => {
     if (!formIsValid) {
       return;
     }
-
+    
+    props.onConfirm({
+        name: enteredName,
+        street: enteredStreet,
+        city: enteredCity,
+        ziCode: enteredZipCode
+    });
     //   submit cart data
   };
 
